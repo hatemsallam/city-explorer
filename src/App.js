3e6ac676d9fbdc283a5b564dc9786f00f3a2649e
hatemsallam <hatemsallam94@hotmail.com>
2021-08-03 00:29:58 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Map from './components/Map';
+import Weather from './components/Weather';
 
 
 
@@ -17,17 +18,43 @@ class App extends React.Component {
       showMap: false,
       errorMsg: 'bad response',
       displayErr: false,
-      showCard: false
+      showCard: false,
+      weather : ''
     }
   }
-  getLocationData = async (event) => {
+  getLocationandWeatherData = async (event) => {
     event.preventDefault();
     let cityName = event.target.city.value;
     console.log(cityName);
     let Url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${cityName}&format=json`;
+    let URL = '';
+
+
+
+    if(cityName === 'amman' || cityName === 'AMMAN')
+    {
+      URL = `${process.env.REACT_APP_SERVER_URL}/weather?lat=31.95&lon=35.91&searchQuery=Amman`;
+    }
+    else if(cityName === 'paris' || cityName === 'PARIS')
+    {
+      URL = `${process.env.REACT_APP_SERVER_URL}/weather?lat=48.86&lon=2.35&searchQuery=Paris`;
+    }
+    else if(cityName === 'seattle' || cityName === 'SEATTLE')
+    {
+      URL = `${process.env.REACT_APP_SERVER_URL}/weather?lat=47.60621&lon=-122.33207&searchQuery=Seattle`;
+    }
+
+
+
+
 
     try {
       let locResult = await axios.get(Url)
+
+
+      
+
+      let weatherData =  axios.get(URL);
 
       this.setState(
         {
@@ -35,7 +62,8 @@ class App extends React.Component {
           lon: locResult.data[0].lon,
           lat: locResult.data[0].lat,
           showMap: true,
-          showCard: true
+          showCard: true,
+          weather : weatherData.data
 
         }
       )
@@ -51,24 +79,29 @@ class App extends React.Component {
       })
     }
 
+  
+
+   
+
+
+
+
   }
+
 
   render() {
     return (
       <>
         <h1>City Explorer</h1>
-        <form onSubmit={this.getLocationData}>
+        <form onSubmit={this.getLocationandWeatherData}>
           <input type='text' placeholder='Enter City' name='city' />
           <button type='submit'>Explore!</button>
         </form>
 
 
-
-
-
         <Map displayName={this.state.displayName} showMap={this.state.showMap} lat={this.state.lat} lon={this.state.lon} displayErr={this.state.displayErr} errorMsg={this.state.errorMsg} showCard={this.state.showCard} ></Map>
 
-
+         <Weather weather={this.state.weather}  ></Weather>
 
 
 
